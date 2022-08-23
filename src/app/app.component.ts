baceb8bd093dd9685178ea678e6482322c744639
Sheeshpal Singh <123sheeshpalsinghsaini@gmail.com>
2022-08-23 18:02:58 +0530

    day one done

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',     // we targeted component by this
+  templateUrl: './app.component.html',  // target html
+  styleUrls: ['./app.component.css']    //target css
+})
+export class AppComponent {
+  title = 'angular-ecom';
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+//this is main module,
+/*
+  where we mention add all components
+*/
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
